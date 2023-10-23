88847a8459a8925a20bb5d69780d753200eed006
yossularko <radenlarko@gmail.com>
2023-10-23 22:35:39 +0700

    test: data store

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {MyButton} from '../../components';
+import {useAtomValue, useSetAtom} from 'jotai';
+import {signOutAtom, userDataAtom} from '../../store/mainStore';
 
 const Home = () => {
+  const userData = useAtomValue(userDataAtom)
+  const signOut = useSetAtom(signOutAtom);
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>Welcome, {userData.name}</Text>
+      <MyButton onPress={signOut} title="Logout" />
     </View>
   );
 };
@@ -12,5 +18,5 @@ const Home = () => {
 export default Home;
 
 const styles = StyleSheet.create({
-  container: {flex: 1},
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
 });
