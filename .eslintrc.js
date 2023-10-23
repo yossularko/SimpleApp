44030311d1757160b766ab7d9c67d333ea6d6cd9
yossularko <radenlarko@gmail.com>
2023-10-23 22:31:57 +0700

    update: eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      rules: {
+        'react-native/no-inline-styles': 0,
+        'prettier/prettier': [
+          'error',
+          {
+            'no-inline-styles': false,
+          },
+        ],
+      },
+    },
+  ],
 };
