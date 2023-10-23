5c71cdc85d0f0d4cd0c8b3c6a2df4104b598cfc1
yossularko <radenlarko@gmail.com>
2023-10-23 22:04:49 +0700

    update: splash screen

--- src/screens/Auth/index.tsx
+++ src/screens/Auth/index.tsx
@@ -1,10 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
 import React from 'react';
 
 const Splash = () => {
   return (
     <View style={styles.container}>
-      <Text>Splash</Text>
+      <ActivityIndicator size="large" color="gray" />
+      <Text>App Loading..</Text>
     </View>
   );
 };
@@ -12,5 +13,5 @@ const Splash = () => {
 export default Splash;
 
 const styles = StyleSheet.create({
-  container: {flex: 1},
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
 });
